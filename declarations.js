a02836cdd6daf5cb033c3fbdf8e49c3e33830101
rodnochieng <rodneyotieno27@gmail.com>
2024-09-30 11:38:12 +0300

    add const variables and freexe

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = "\`, \\, /, \"\'"
+const arr=[4,'2']
+const obj={
+    str:"string",
+    num:1,
+    bool:true,
+    undef:undef
+}
+const nested={
+    arr: [4,undefined,'2'],
+    obj: {
+        str:"hello",
+        num:1,
+        bool:true
+    }
+}
+Object.freeze(arr)
+Object.freeze(nested)
+Object.freeze(obj)
