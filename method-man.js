9ce64c5d55183e0258135638b0c8200b2ce1d239
rod31 <rodneyotieno27@gmail.com>
2024-09-30 21:42:41 +0300

    complette capitalize,yell and whisper funcctions

--- method-man.js
+++ method-man.js
@@ -1,6 +1,16 @@
 const words=(str)=>{return str.split(" ")}
+
 const sentence=(arr)=>{
     let result=[]
     arr.forEach(array=>result.push(...(array.split(" "))))
     return result
+}
+
+const yell=(str)=>{return str.toUpperCase()}
+
+const whisper=(str)=>{return "*"+str.toLowerCase()+"*"}
+
+const capitalize=(str)=>{str=str.toLowerCase()
+    let ch=str.charAt(0).toUpperCase()
+    return ch+str.slice(1)
 }
