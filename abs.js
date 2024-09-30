d0972224d1a50bb9b5c2b8dd5ca8068c0c8bdd35
rod31 <rodneyotieno27@gmail.com>
2024-09-30 22:52:18 +0300

    fix  abs function

--- abs.js
+++ abs.js
@@ -2,6 +2,10 @@ const isPositive=(num)=>{
     return num>0 
 }
 
-function abs(num){
+function abs( num ){
+   if (num ==0) {
+    return 0
+   }
     return num>0 ? num : -num;
 }
+console.log(abs(0))
