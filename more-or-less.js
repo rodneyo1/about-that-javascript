c66028de417e489d974e57d20b65792c7a04f3dd
rodnochieng <rodneyotieno27@gmail.com>
2024-09-30 13:09:39 +0300

    implement arrow functions and sub function

---
+++ more-or-less.js
@@ -0,0 +1,13 @@
+const more=(num)=>{
+   return  num+1
+}
+const less=(num)=>{
+    return num-1
+}
+const add=(num1,num2)=>{
+    return num1+num2
+}
+
+function sub(num1,num2){
+    return num1-num2
+}
