cd3331e0e1cf850fcb05382594d63962bc78da4a
rodnochieng <rodneyotieno27@gmail.com>
2024-10-01 13:12:37 +0300

    remove type coercien is is.def

--- is.js
+++ is.js
@@ -3,7 +3,7 @@ is.nan=function nan (num){return Number.isNaN(num)}
 is.bool=function bool (num){return typeof(num)==="boolean"}
 is.str=function str (num){return typeof(num)==="string"}
 is.undef=function undef (num){return num===undefined}
-is.def=function def (num){return num!=undefined}
+is.def=function def (num){return num!==undefined}
 is.arr=function arr (num){return Array.isArray(num)}
 is.obj=function obj (num){return typeof(num)==="object" && num!=null && !Array.isArray(num)}
 is.fun=function fun (num){return typeof(num)==="function"}
