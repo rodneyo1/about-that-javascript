a76be78b11938128a0f49b4c1493694909a65dbc
rodnochieng <rodneyotieno27@gmail.com>
2024-10-01 12:52:55 +0300

    create object methods for is object

---
+++ is.js
@@ -0,0 +1,15 @@
+is.num=function num (num){return typeof(num)==="number"}
+is.nan=function nan (num){return isNaN(num)}
+is.bool=function bool (num){return typeof(num)==="boolean"}
+is.str=function str (num){return typeof(num)==="string"}
+is.undef=function undef (num){return num===undefined}
+is.def=function def (num){return num!=undefined}
+is.arr=function arr (num){return Array.isArray(num)}
+is.obj=function obj (num){return typeof(num)==="object"}
+is.fun=function fun (num){return typeof(num)==="function"}
+is.truthy=function truthy (num){ 
+   return num? true:false
+}
+is.falsy=function falsy(num){
+    return !num? true:false
+}
