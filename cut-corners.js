d18369bc73d3be1cbae0596aaccbf3c543441810
rodnochieng <rodneyotieno27@gmail.com>
2024-10-02 17:02:10 +0300

    correct calculations

--- cut-corners.js
+++ cut-corners.js
@@ -1,35 +1,35 @@
 const trunc = (num) => {
-    if (num >= 0) {
-      return num - (num + 1000000) + 1000000;
-    } else {
-      return num + ((-num) + 1000000) - 1000000;
+    const strNum = String(num);
+    let decimalIndex = strNum.indexOf('.');
+    
+    if (decimalIndex === -1) {
+        return num; // No decimal point, it's already truncated.
     }
-  };
-  
-  const floor = (num) => {
+    
+    return parseFloat(strNum.slice(0, decimalIndex)); // Remove decimal part.
+};
+
+const floor = (num) => {
     const truncated = trunc(num);
     return num < truncated ? truncated - 1 : truncated;
-  };
-  
-  const ceil = (num) => {
+};
+
+const ceil = (num) => {
     const truncated = trunc(num);
     return num > truncated ? truncated + 1 : truncated;
-  };
-  
-  const round = (num) => {
-    const truncated = trunc(num);
-    const fractional = num - truncated;
+};
+
+const round = (num) => {
     if (num >= 0) {
-      return fractional >= 0.5 ? truncated + 1 : truncated;
+        return floor(num + 0.5);
     } else {
-      return fractional > -0.5 ? truncated : truncated - 1;
+        return ceil(num - 0.5);
     }
-  };
-  
-  
-//   // Test the functions
-//   const nums = [3.7, -3.7, 3.1, -3.1];
-//   console.log(nums.map(round));
-//   console.log(nums.map(floor));
-//   console.log(nums.map(trunc));
-//   console.log(nums.map(ceil));
+};
+
+// // Test the functions
+// const nums = [3.7, -3.7, 3.1, -3.1];
+// console.log(nums.map(round));
+// console.log(nums.map(floor));
+// console.log(nums.map(trunc));
+// console.log(nums.map(ceil));
