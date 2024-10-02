e428f9b9d99493518f67fb5286f76ac7649470f1
rodnochieng <rodneyotieno27@gmail.com>
2024-10-02 16:42:45 +0300

    remove nums declaration

--- cut-corners.js
+++ cut-corners.js
@@ -23,7 +23,7 @@ const round = (num) => {
   };
   
   // Test the functions
-  const nums = [3.7, -3.7, 3.1, -3.1];
+ // const nums = [3.7, -3.7, 3.1, -3.1];
   console.log(nums.map(round));
   console.log(nums.map(floor));
   console.log(nums.map(trunc));
