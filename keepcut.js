8925dfac1c80183f1ad963595675cedf0df210d2
rodnochieng <rodneyotieno27@gmail.com>
2024-10-02 15:46:20 +0300

    initialize keepcut

---
+++ keepcut.js
@@ -0,0 +1,44 @@
+function cutFirst(str){
+    result=""
+    for (let i = 2; i < str.length; i++) {
+       result=result+str[i]
+      }
+    return result
+}
+
+function cutLast(str){
+    result=""
+    for (let i = 0; i < str.length-2; i++) {
+       result=result+str[i]
+      }
+    return result
+}
+
+function cutFirstLast(str){
+   let st=cutFirst(str)
+    return (cutLast(st))
+}
+
+function keepFirst(str){
+    if (str.length<=2){
+        return str
+    }  
+    return str[0]+str[1]
+}
+
+function keepLast(str){
+    let n=str.length
+    if (str.length<=2){
+        return str
+    } 
+    return str[n-2]+str[n-1]
+}
+
+function keepFirstLast(str){
+    let n=str.length
+    if (n<4){
+        return str
+    }
+ return str[0]+str[1]+str[n-2]+str[n-1]
+}
+//console.log(keepFirstLast("ft"))
