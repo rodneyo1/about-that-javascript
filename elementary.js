c346807d371509ebdead266a3df50604ec261521
rodnochieng <rodneyotieno27@gmail.com>
2024-10-02 12:29:33 +0300

    remove operators

--- elementary.js
+++ elementary.js
@@ -1,3 +1,30 @@
-const multiply=(a,b)=>{return a*b}
-const divide=(a,b)=>{return a/b}
-const modulo=(a,b)=>{ return a%b}
+const multiply=(a,b)=>{
+    let res=0
+    while (b > 0){
+         res+=a
+         b--
+    }
+    return res
+    
+}
+const divide=(a,b)=>{
+    //positives
+    let res=0
+    while (a-b >= 0){
+         a-=b
+         res++
+    }
+    return res
+    
+}
+
+const modulo=(a,b)=>{ 
+    //positives
+    let res=0
+    while (a-b >= 0){
+        a-=b
+        res++
+   }
+   return a
+   
+}
