296d2779ebda71eec141fdd219b58282aeb88712
rod31 <rodneyotieno27@gmail.com>
2024-10-03 12:29:16 +0300

    correct reverse;

--- reverser.js
+++ reverser.js
@@ -1,12 +1,12 @@
 function reverse(arr) {
-  let n = arr.length - 1;
-  let res = [];
-  for (let i = 0; i <= n; i++) {
-    res.push(arr[i]);
+    let n = arr.length - 1;
+    let res = [];
+    for (let i = n; i >= 0; i--) {
+      res.push(arr[i]);
+    }
+    return res;
   }
-  return res;
-}
-Array.prototype.reverse = reverse
+//Array.prototype.reverse = reverse
 // let a=[1, 2, 3]
 // console.log(a.reverse())
 // console.log(reverse(a))
