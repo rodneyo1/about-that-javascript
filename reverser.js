510ee147b500d8709f93852dbe27d73e618423de
rod31 <rodneyotieno27@gmail.com>
2024-10-03 12:37:16 +0300

    handle strings

--- reverser.js
+++ reverser.js
@@ -1,19 +1,16 @@
-function reverse(input) {
-    if (typeof input === 'string') {
-      // Convert the string to an array, reverse it, and join it back into a string
-      return input.split('').reverse().join('');
-    } else if (Array.isArray(input)) {
-      // Handle array reversal
-      let n = input.length - 1;
-      let res = [];
-      for (let i = n; i >= 0; i--) {
-        res.push(input[i]);
-      }
-      return res;
+function reverse(arr) {
+    let n = arr.length - 1;
+    let res = ""
+    Array.isArray(arr)?res=[]:res
+
+    for (let i = n; i >= 0; i--) {
+
+        Array.isArray(arr)?res.push(arr[i]):res+=arr[i]
+      
     }
+    return res;
   }
-  
 //Array.prototype.reverse = reverse
-// let a=[1, 2, 3]
+// let a=["lktr"]
 // console.log(a.reverse())
 // console.log(reverse(a))
