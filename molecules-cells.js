dcb121320e4c1bd39a925a39b2b140e195e5374b
rod31 <rodneyotieno27@gmail.com>
2024-10-03 13:49:10 +0300

    fix rna ,dna fuctions

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,7 +1,13 @@
 function RNA(dna) {
-    return dna.replace(/T/g, 'U');
+   dna= dna.replace(/A/g, 'U');
+   dna= dna.replace(/T/g, 'A');
+    return dna;
   }
   
   function DNA(rna) {
-    return rna.replace(/U/g, 'T');
+   rna= rna.replace(/A/g, 'T');
+   rna= rna.replace(/U/g, 'A');
+    return rna;
   }
+
+//   console.log(RNA('TAGC'))
