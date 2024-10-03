de01c104a90ccbb000a8e2827339a58316223abc
rod31 <rodneyotieno27@gmail.com>
2024-10-03 12:18:28 +0300

    initialize reverse function

---
+++ reverser.js
@@ -0,0 +1,12 @@
+function reverse(arr){
+    n=arr.length-1
+    let res=[]
+     for (let i=0; i<=n; i++){
+        res.push(arr[i])
+     }
+     return res
+}
+
+// let a=[3,4,5,6,7,,8,9,10,11,12]
+// console.log(a.reverse())
+// console.log(reverse(a))
