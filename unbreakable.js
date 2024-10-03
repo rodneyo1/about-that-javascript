664def6e2b21cd0e583b5415c66d15f216efcb46
rod31 <rodneyotieno27@gmail.com>
2024-10-03 12:50:46 +0300

    correct consecutives

--- unbreakable.js
+++ unbreakable.js
@@ -13,13 +13,18 @@ function split(s, sep) {
         result.push(s.slice(start, i));
         // Update the start index for the next substring
         start = i + sep.length;
+        // Skip ahead by the length of the separator to avoid overlapping
+        i += sep.length - 1;
       }
     }
   
     // Append the last substring (after the final separator) to the result
-    result.push(s.slice(start));
-    
+    if (start < s.length || s.endsWith(sep)) {
+      result.push(s.slice(start));
+    }
+  
     return result;
   }
   
-//   console.log(split("hello world go","o"))
+  
+//   console.log(split("rrrr","rr"))
