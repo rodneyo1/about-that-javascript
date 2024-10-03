e350bb44191b655c30c9f3f8b9abed45e7896f16
rod31 <rodneyotieno27@gmail.com>
2024-10-03 12:19:42 +0300

    define n

--- reverser.js
+++ reverser.js
@@ -1,10 +1,10 @@
-function reverse(arr){
-    n=arr.length-1
-    let res=[]
-     for (let i=0; i<=n; i++){
-        res.push(arr[i])
-     }
-     return res
+function reverse(arr) {
+  let n = arr.length - 1;
+  let res = [];
+  for (let i = 0; i <= n; i++) {
+    res.push(arr[i]);
+  }
+  return res;
 }
 
 // let a=[3,4,5,6,7,,8,9,10,11,12]
