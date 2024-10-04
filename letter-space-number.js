0bd48804724e5a32755f0b4692ef479d42a1e62e
rod31 <rodneyotieno27@gmail.com>
2024-10-04 13:37:13 +0300

    fix letterspace function

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,9 +1,13 @@
 function letterSpaceNumber(str) {
-    const regex = /\b[a-zA-Z]\s\d\b/;
+    // Regular expression to match a letter followed by a space and a single-digit number not followed by any letter
+    const regex = /[a-zA-Z] \d(?![a-zA-Z])/g;
     const matches = str.match(regex);
-    return Array.from(matches);
-  }
-  
+
+    // Return the matches, or an empty array if there are none
+    return matches || [];
+}
+
 // console.log(letterSpaceNumber("m 1n 2o 345"));
 // console.log(letterSpaceNumber("a 1 b 2c 3"));
 // console.log(letterSpaceNumber("x 4y 5 z 9"));
+
