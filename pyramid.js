29db97d91cf02e6b2832362c5050eb2439613d6e
rod31 <rodneyotieno27@gmail.com>
2024-10-04 11:09:35 +0300

    use prepend for newlines

--- pyramid.js
+++ pyramid.js
@@ -4,12 +4,15 @@ function pyramid(char, height) {
   for (let i = 1; i <= height; i++) {
     const spaces = ' '.repeat(height - i); // Add spaces to center the pyramid
     const chars = char.repeat(i * 2 - 1);  // Characters increase by 2 in each row
-    result += spaces + chars;
 
-    if (i !== height) {
-      result += '\n'; // Only add newline if it's not the last row
+    if (i > 1) {
+      result += '\n'; // Prepend newline for rows after the first one
     }
+
+    result += spaces + chars;
   }
 
   return result;
 }
+
+console.log(pyramid('*', 5));
