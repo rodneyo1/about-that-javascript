f2fe6087d19fbe5a87aa2b215895ce3aff8f531f
rod31 <rodneyotieno27@gmail.com>
2024-10-04 14:12:56 +0300

    make input regex global

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,10 @@
 function sameAmount(str, regex1, regex2) {
+    regex1 = new RegExp(regex1, "g")
+    regex2 = new RegExp(regex2, "g")
     const match1 = str.match(regex1) || []
     const match2 = str.match(regex2) || []
     return match1.length === match2.length
 }
 
-// console.log(sameAmount("hello world", /l/g, /o/g))
-// console.log(sameAmount("ababab", /a/g, /b/g))
+console.log(sameAmount("hello world", /l/g, /o/g))
+console.log(sameAmount("ababab", /a/g, /b/g))
