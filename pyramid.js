c2466035c3154c3a28115ef79682b1410ba6e0ea
rod31 <rodneyotieno27@gmail.com>
2024-10-04 11:05:47 +0300

    remove newline from last line

--- pyramid.js
+++ pyramid.js
@@ -1,16 +1,15 @@
 function pyramid(char, height) {
-    let result = '';
-  
-    for (let i = 1; i <= height; i++) {
-      const spaces = ' '.repeat(height - i); // Add spaces to center the pyramid
-      const chars = char.repeat(i * 2 - 1);  // Characters increase by 2 in each row
-      result += spaces + chars;
-  
-      if (i !== height) {
-        result += '\n'; // Add a new line after each row, except the last one
-      }
+  let result = '';
+
+  for (let i = 1; i <= height; i++) {
+    const spaces = ' '.repeat(height - i); // Add spaces to center the pyramid
+    const chars = char.repeat(i * 2 - 1);  // Characters increase by 2 in each row
+    result += spaces + chars;
+
+    if (i !== height) {
+      result += '\n'; // Only add newline if it's not the last row
     }
-  
-    return result;
   }
-  console.log(pyramid('*', 5));
+
+  return result;
+}
