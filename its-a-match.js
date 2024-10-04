07bd7d3db860d6dfd4270e09d61edd35907f0416
rod31 <rodneyotieno27@gmail.com>
2024-10-04 13:13:01 +0300

    add regex for hi

---
+++ its-a-match.js
@@ -0,0 +1,4 @@
+const normal = /hi/;        //  'hi' anywhere
+const begin = /^hi/;        //  'hi' at  start
+const end = /hi$/;          // 'hi' at  end
+const beginEnd = /^hi$/;    // exactly 'hi'
