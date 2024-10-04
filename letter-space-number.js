622bd2650027288c22ea04ef8db428bdef5a79c2
rod31 <rodneyotieno27@gmail.com>
2024-10-04 13:31:29 +0300

    remove matchall

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,10 +1,9 @@
 function letterSpaceNumber(str) {
     const regex = /\b[a-zA-Z]\s\d\b/;
-    const matches = str.matchAll(regex);
+    const matches = str.match(regex);
     return Array.from(matches);
   }
   
 // console.log(letterSpaceNumber("m 1n 2o 345"));
 // console.log(letterSpaceNumber("a 1 b 2c 3"));
 // console.log(letterSpaceNumber("x 4y 5 z 9"));
-
