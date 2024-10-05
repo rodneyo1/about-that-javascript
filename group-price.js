a80da17a0b1a94fb4637bd8c0110d5ebe6f1d0fa
rod31 <rodneyotieno27@gmail.com>
2024-10-05 23:25:25 +0300

    account for $ sign

--- group-price.js
+++ group-price.js
@@ -1,5 +1,5 @@
 function groupPrice(dataSet) {
-    const pricePattern = /([A-Z]{3})(\d+)\.(\d{2})/g;
+    const pricePattern = /([$A-Z]{1,3})(\d+)\.(\d{2})/g;
     let result = [];
     let match;
   
@@ -11,7 +11,9 @@ function groupPrice(dataSet) {
   }
   
   // Example usage:
-  const dataSet = "The price is USD12.31, and another item costs EUR45.67.";
-  console.log(groupPrice(dataSet));
+  console.log(groupPrice('The price of the cereals is $4.00.'));
+  // Output: [ [ '$4.00', '4', '00' ] ]
+  
+  console.log(groupPrice('The price is USD12.31, and another item costs EUR45.67.'));
   // Output: [ [ 'USD12.31', '12', '31' ], [ 'EUR45.67', '45', '67' ] ]
   
