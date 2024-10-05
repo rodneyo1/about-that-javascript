dc1dbe64e94cbd32331726892d63b5b5ac003a9e
rod31 <rodneyotieno27@gmail.com>
2024-10-05 22:59:52 +0300

    init geturl function

---
+++ greedy-url.js
@@ -0,0 +1,28 @@
+function getURL(dataSet) {
+    return dataSet.match(/\bhttps?:\/\/[^\s]+/g) || [];
+  }
+  
+//   function greedyQuery(dataSet) {
+//     return getURL(dataSet).filter(url => (url.match(/\?/g) && url.split('?')[1].split('&').length >= 3));
+//   }
+  
+//   function notSoGreedy(dataSet) {
+//     return getURL(dataSet).filter(url => {
+//       const queryParams = url.split('?')[1]?.split('&').length;
+//       return queryParams >= 2 && queryParams <= 3;
+//     });
+//   }
+  
+//   // Example dataSet:
+//   const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you&extra=1&another=2";
+  
+//   // Test the functions:
+//   console.log(getURL(dataSet));
+//   // Output: ['https://something.com/hello', 'http://example.com/hello?you=something&something=you&extra=1&another=2']
+  
+//   console.log(greedyQuery(dataSet));
+//   // Output: ['http://example.com/hello?you=something&something=you&extra=1&another=2']
+  
+//   console.log(notSoGreedy(dataSet));
+//   // Output: []
+  
