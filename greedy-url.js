259e0e647622e27a36576325b99b817b743ce4aa
rod31 <rodneyotieno27@gmail.com>
2024-10-05 23:17:37 +0300

    fix query

--- greedy-url.js
+++ greedy-url.js
@@ -1,21 +1,32 @@
+const urlExp = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g;
 
 function getURL(dataSet) {
-  return dataSet.match(/\bhttps?:\/\/[^\s]+/g) || [];
+  return dataSet.match(urlExp) || [];
 }
 
 function greedyQuery(dataSet) {
-  return getURL(dataSet).filter(url => (url.match(/\?/g) && url.split('?')[1].split('&').length >= 3));
+  return getURL(dataSet).filter((url) => {
+    // Looks for at least 3 query parameters (2 & and the final one)
+    return /\?([^&=]+=[^&=]*&){2,}/.test(url);
+  });
 }
 
 function notSoGreedy(dataSet) {
-  return getURL(dataSet).filter(url => {
-    const queryParams = url.split('?')[1]?.split('&').length;
-    return queryParams >= 2 && queryParams <= 3;
+  return getURL(dataSet).filter((url) => {
+    // Looks for exactly 2 or 3 query parameters (1 or 2 & and the final one)
+    return /\?([^&=]+=[^&=]*&){1,2}[^&=]+=[^&=]*$/.test(url);
   });
 }
-// // Example dataSet
-// const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you";
 
-// console.log("getURL:", getURL(dataSet));
-// console.log("greedyQuery:", greedyQuery(dataSet));
-// console.log("notSoGreedy:", notSoGreedy(dataSet));
+// // Example dataSet:
+// const dataSet = "qqq http:// qqqq q qqqqq https://something.com/hello qqqqqqq qhttp://example.com/hello?you=something&something=you&extra=1&another=2";
+
+// // Test the functions:
+// console.log(getURL(dataSet));
+// // Output: ['https://something.com/hello', 'http://example.com/hello?you=something&something=you&extra=1&another=2']
+
+// console.log(greedyQuery(dataSet));
+// // Output: ['http://example.com/hello?you=something&something=you&extra=1&another=2']
+
+// console.log(notSoGreedy(dataSet));
+// // Output: []
