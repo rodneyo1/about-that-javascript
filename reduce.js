fe83ddce3c76ca277c388ac6e2d968facc9778c3
rodnochieng <rodneyotieno27@gmail.com>
2024-10-09 16:02:56 +0300

    fix error message

--- reduce.js
+++ reduce.js
@@ -17,7 +17,7 @@ function foldRight(arr, func, accumulator) {
 // Reduce function
 function reduce(arr, func) {
     if (arr.length < 1) {
-        throw new Error('Array must have at least one element.');
+        throw new Error('Add elements to array');
     }
 
     let accumulator = arr[0]
@@ -30,7 +30,7 @@ function reduce(arr, func) {
 // ReduceRight function
 function reduceRight(arr, func) {
     if (arr.length < 1) {
-        throw new Error('Array must have at least one element.');
+        throw new Error('Add elements to array.');
     }
 
     let accumulator = arr[arr.length - 1]; 
@@ -40,10 +40,10 @@ function reduceRight(arr, func) {
     return accumulator;
 }
 
-// Example usage:
-const adder = (a, b) => a + b;
+// // Example usage:
+// const adder = (a, b) => a + b;
 
-console.log(fold([1, 2, 3], adder, 2));           // 8 (2 + 1 + 2 + 3)
-console.log(foldRight([1, 2, 3], adder, 2));      // 8 (2 + 3 + 2 + 1)
-console.log(reduce([1, 2, 3], adder));            // 6 (1 + 2 + 3)
-console.log(reduceRight([1, 2, 3], adder));       // 6 (3 + 2 + 1)
+// console.log(fold([1, 2, 3], adder, 2));           // 8 (2 + 1 + 2 + 3)
+// console.log(foldRight([1, 2, 3], adder, 2));      // 8 (2 + 3 + 2 + 1)
+// console.log(reduce([1, 2, 3], adder));            // 6 (1 + 2 + 3)
+// console.log(reduceRight([1, 2, 3], adder));       // 6 (3 + 2 + 1)
