1def3179be0909cd4ddbe24df56387cdfe3972cb
rodnochieng <rodneyotieno27@gmail.com>
2024-10-11 15:16:35 +0300

    fix generator

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,18 +1,21 @@
 function generateLetters() {
+    
     for (let i = 0; i < 120; i++) {
-        let letter = document.createElement("div");
+
+        let letter = document.createElement("div")
         letter.style.fontSize = `${11 + i}px`;
         letter.textContent = String.fromCharCode(
             65 + Math.floor(Math.random() * 26)
-        );
+        )
         if (i < 40) {
-            letter.style.fontWeight = "300";
+            letter.style.fontWeight = "300"
         } else if (i < 80) {
-            letter.style.fontWeight = "400";
+            letter.style.fontWeight = "400"
         } else {
-            letter.style.fontWeight = "600";
+            letter.style.fontWeight = "600"
         }
         document.getElementsByTagName("body")[0].appendChild(letter);
+
     }
 }
 
