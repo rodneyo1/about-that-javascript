d12521721c55eae451901e1507c97cf810e44c4f
rodnochieng <rodneyotieno27@gmail.com>
2024-10-11 15:13:39 +0300

    fix

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,32 +1,21 @@
-// harder-bigger-bolder-stronger.js
-export function generateLetters() {
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Possible characters
-  const container = document.createElement('div'); // Create a container for the letters
-  
-  for (let i = 0; i < 120; i++) { // Adjust loop to start from 0 for easier calculations
-    let char = possible.charAt(Math.floor(Math.random() * possible.length)); // Random letter
-    let elem = document.createElement('div'); // Create a new div for each letter
-    
-    elem.innerHTML = char; // Set the inner HTML to the random character
+export function generateLetters(){
 
-    // Calculate font-size from 11 to 130 pixels
-    const fontSize = 11 + (i * (130 - 11) / 119);
-    elem.style.fontSize = fontSize + 'px'; // Set font-size
+  let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-    // Set font-weight based on the range
-    if (i < 40) {
-      elem.style.fontWeight = '300'; // First third
-    } else if (i < 80) {
-      elem.style.fontWeight = '400'; // Second third
-    } else {
-      elem.style.fontWeight = '600'; // Last third
-    }
-    
-    container.appendChild(elem); // Append each letter div to the container
+  for(let i=1;i<=120;i++){
+      let char = ''
+      char += letters.charAt(Math.floor(Math.random()*letters.length))
+      let element = document.createelementent('div')
+      element.innerHTML = char
+      element.style.fontSize = (i+10)+'px'
+      if (i <= 40 ){
+          element.style.fontWeight = "300"
+      } else if (i <= 80) {
+          element.style.fontWeight = "400"
+      } else {
+          element.style.fontWeight = "600"
+      }
+      document.body.append(element)
+      console.log(i)
   }
-
-  document.body.appendChild(container); // Append the container to the body
-
-  // Log the number of letter divs created
-  console.log(`Number of letter divs created: ${container.children.length}`); // Should log 120
 }
