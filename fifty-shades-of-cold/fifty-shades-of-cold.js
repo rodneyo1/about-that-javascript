1bf6e7686f8485d9a0a9c3ff4ed89f9e1f839457
rod31 <rodneyotieno27@gmail.com>
2024-10-12 13:52:47 +0300

    initialize functions and format with prettier

---
+++ fifty-shades-of-cold/fifty-shades-of-cold.js
@@ -0,0 +1,28 @@
+import { colors } from "./fifty-shades-of-cold.data.js";
+
+export const generateClasses = () => {
+  const style = document.createElement("style");
+  for (const color of colors) {
+    style.innerHTML += `.${color} {\n   background: ${color};\n}\n`;
+  }
+  document.querySelector("head").append(style);
+};
+
+// export const generateColdShades = () => {
+//   const cold = ["aqua", "blue", "turquoise", "green", "cyan", "navy", "purple"];
+//   for (const color of colors) {
+//     for (const coldColor of cold) {
+//       if (color.includes(coldColor)) {
+//         const div = document.createElement("div");
+//         div.classList.add(color);
+//         div.textContent = color;
+//         document.querySelector("body").append(div);
+//       }
+//     }
+//   }
+// };
+
+export const choseShade = (color) => {
+  const alldivs = [...document.querySelectorAll("div")];
+  alldivs.map((div) => (div.className = color));
+};
