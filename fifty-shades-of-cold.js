7268922c16e77407c1d7fc63d55547553eb1d25a
rod31 <rodneyotieno27@gmail.com>
2024-10-12 13:54:09 +0300

    fix generate coldshades function

--- fifty-shades-of-cold.js
+++ fifty-shades-of-cold.js
@@ -8,21 +8,21 @@ export const generateClasses = () => {
   document.querySelector("head").append(style);
 };
 
-// export const generateColdShades = () => {
-//   const cold = ["aqua", "blue", "turquoise", "green", "cyan", "navy", "purple"];
-//   for (const color of colors) {
-//     for (const coldColor of cold) {
-//       if (color.includes(coldColor)) {
-//         const div = document.createElement("div");
-//         div.classList.add(color);
-//         div.textContent = color;
-//         document.querySelector("body").append(div);
-//       }
-//     }
-//   }
-// };
-
-export const choseShade = (color) => {
-  const alldivs = [...document.querySelectorAll("div")];
-  alldivs.map((div) => (div.className = color));
+export const generateColdShades = () => {
+  const cold = ["aqua", "blue", "turquoise", "green", "cyan", "navy", "purple"];
+  for (const color of colors) {
+    for (const coldColor of cold) {
+      if (color.includes(coldColor)) {
+        const div = document.createElement("div");
+        div.classList.add(color);
+        div.textContent = color;
+        document.querySelector("body").append(div);
+      }
+    }
+  }
 };
+
+// export const choseShade = (color) => {
+//   const alldivs = [...document.querySelectorAll("div")];
+//   alldivs.map((div) => (div.className = color));
+// };
