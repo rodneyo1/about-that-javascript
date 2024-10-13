222f1c4677b92efa1f12aa07ca0e0e679d8729d7
rod31 <rodneyotieno27@gmail.com>
2024-10-13 07:49:55 +0300

    fix asciii condition

--- keycodes-symphony.js
+++ keycodes-symphony.js
@@ -145,23 +145,23 @@ const colors = [
 export const compose = () => {
     document.addEventListener('keydown', function(e) {
         console.log(e.key)
-        const keyAscii = e.key.charCodeAt(0)
-        if (keyAscii >= 97 && keyAscii <= 122) {
+        const key = e.key.charCodeAt(0)
+        if (key > 96 && key < 123) {
             const div = document.createElement('div')
             div.textContent = e.key
             div.classList.add("note")
-            div.style.backgroundColor = colors[122-keyAscii]
+            div.style.backgroundColor = colors[122-key]
             document.querySelector('body').append(div)
-        }
-        if (e.key === "Backspace") {
-            const divs = document.getElementsByClassName("note")
-            divs[divs.length-1]?.remove()
         }
         if (e.key === "Escape") {
             const divs = document.getElementsByClassName("note")
             for (let i = divs.length-1; i >= 0; i--) {
                 divs[i].remove()
             }
+        }
+         if (e.key === "Backspace") {
+            const divs = document.getElementsByClassName("note")
+            divs[divs.length-1]?.remove()
         }
     })
 }
