b3731aaac524ab71fae980708347ff0da0b4f491
rodnochieng <rodneyotieno27@gmail.com>
2024-10-14 12:02:57 +0300

    more test cases

--- fusion.js
+++ fusion.js
@@ -24,11 +24,27 @@ function fusion(obj1, obj2) {
 
   const nutrients = { carbohydrates: 12, protein: 20, fat: 5 }
 
-console.log(filterValues(nutrients, (nutrient) => nutrient <= 12))
-// output: { carbohydrates: 12, fat: 5 }
 
-console.log(mapValues(nutrients, (v) => v+1))
-// output: { carbohydrates: 13, protein: 21, fat: 6 }
+  //For array types, you will concatenate them.
 
-console.log(reduceValues(nutrients, (acc, cr) => acc + cr))
-// output: 37
+  fusion({ arr: [1, "2"] }, { arr: [2] }); // -> { arr: [ 1, '2', 2 ] }
+  fusion({ arr: [], arr1: [5] },{ arr: [10, 3], arr1: [15, 3], arr2: ["7", "1"] }); // ->{ arr: [ 10, 3 ], arr1: [ 5, 15, 3 ], arr2: [ '7', '1' ] }
+  
+     // For strings, you must concatenate them with a space.
+  
+  fusion({ str: "salem" }, { str: "alem" }); // -> { str: 'salem alem' }
+  fusion({ str: "salem" }, { str: "" }); // -> { str: 'salem ' }
+  
+     // If they are numbers, you must add them.
+  
+  fusion({ a: 10, b: 8, c: 1 }, { a: 10, b: 2 }); // -> { a: 20, b: 10, c: 1 }
+  
+     // If it is an object, you must join them recursively.
+  
+  fusion({ a: 1, b: { c: "Salem" } }, { a: 10, x: [], b: { c: "alem" } }); // -> { a: 11, x: [], b: { c: 'Salem alem' } }
+  fusion( { a: { b: [3, 2], c: { d: 8 } } },{ a: { b: [0, 3, 1], c: { d: 3 } } }); // -> { a: { b: [ 3, 2, 0, 3, 1 ], c: { d: 11 } } }
+  
+    //  In case of type mismatch you must replace it with the value of the second object (if it exists).
+  
+  fusion({ a: "hello", b: [] }, { a: 4 }); // -> { a: 4, b: [] }
+  
