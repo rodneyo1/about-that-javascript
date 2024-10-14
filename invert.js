4cea0de991950fde100a567e227a3a7e5aa6d261
rodnochieng <rodneyotieno27@gmail.com>
2024-10-14 11:15:06 +0300

    create test cases

--- invert.js
+++ invert.js
@@ -1,3 +1,7 @@
 function invert(obj) {
     return Object.entries(obj).reduce((acc, [key, value]) => ({ ...acc, [value]: key }), {});
   }
+
+const originalObject = { a: 1, b: 2, c: 3 };
+const invertedObject = invert(originalObject);
+console.log(invertedObject)
