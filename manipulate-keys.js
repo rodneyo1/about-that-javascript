a602d6f508c3bb08cd204c592253c0ef27a31e55
rodnochieng <rodneyotieno27@gmail.com>
2024-10-16 11:07:27 +0300

    fix reducekeys function

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -1,22 +1,27 @@
-// filterKeys function - filters keys based on the provided predicate
-function filterKeys(obj, predicate) {
-    return Object.fromEntries(Object.entries(obj).filter(([key]) => predicate(key)));
-  }
+const filterKeys = (obj, fn) => {
+    return Object.fromEntries(
+      Object.entries(obj).filter(([key]) => fn(key))
+    );
+  };
   
-  // mapKeys function - maps over the keys and returns a new object with modified keys
-  function mapKeys(obj, callback) {
-    return Object.fromEntries(Object.entries(obj).map(([key, value]) => [callback(key), value]));
-  }
+  const mapKeys = (obj, fn) => {
+    return Object.fromEntries(
+      Object.entries(obj).map(([key, value]) => [fn(key), value])
+    );
+  };
   
-  // reduceKeys function - reduces the keys based on the provided reducer function
-  function reduceKeys(obj, reducer, initialValue = '') {
-    return Object.keys(obj).reduce((acc, key) => {
-      return reducer(acc, key);
-    }, initialValue);
-  }
+  const reduceKeys = (obj, fn, initialValue) => {
+    return Object.keys(obj).reduce(fn, initialValue);
+  };
   
+//   // Example usage:
 //   const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
-//   console.log(filterKeys(nutrients, (key) => /protein/.test(key)));
-//   console.log(mapKeys(nutrients, (k) => `-${k}`));
-//   console.log(reduceKeys(nutrients, (acc, cr) => acc.concat(', ', cr), ''))
   
+//   console.log(filterKeys(nutrients, (key) => /protein/.test(key)));
+//   // Output: { protein: 20 }
+  
+//   console.log(mapKeys(nutrients, (k) => `-${k}`));
+//   // Output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
+  
+//   console.log(reduceKeys(nutrients, (acc, cr) => acc.concat(', ', cr)));
+//   // Output: carbohydrates, protein, fat
