ea559b0eccaa2843009ed67cc5d4fc6bcc0da1cb
rodnochieng <rodneyotieno27@gmail.com>
2024-10-17 14:29:37 +0300

    add interval duration

--- interpolation.js
+++ interpolation.js
@@ -1,24 +1,27 @@
-function interpolation({ step, start, end, duration }) {
-    return new Promise(resolve => {
-      const stepSize = (end - start) / step;
-      const delayBetweenSteps = duration / step;
-  
-      function executeStep(currentStep) {
-        if (currentStep >= step) {
-          resolve([[start + stepSize * (step - 1), duration]]);
-          return;
-        }
-  
-        if (currentStep === step - 1) {
-          const distance = start + stepSize * currentStep;
-          const point = duration;
-          resolve([[distance, point]]);
-          return;
-        }
-  
-        setTimeout(() => executeStep(currentStep + 1), delayBetweenSteps);
+function interpolation(config) {
+    const { step, start, end, callback, duration } = config;
+    
+    const intervalDuration = duration / step;
+    
+    const distanceIncrement = (end - start) / step;
+    
+    let currentStep = 0;
+    
+    const interpolationTimer = setInterval(() => {
+      // current distance
+      const currentDistance = start + (distanceIncrement * currentStep);
+      
+      // Calculate the current time point
+      const currentTimePoint = intervalDuration * (currentStep + 1);
+      
+      // Call the callback with the current interpolated point
+      callback([currentDistance, currentTimePoint]);
+      currentStep++;
+      
+      // Check if we've reached the final step
+      if (currentStep === step) {
+        // Stop the interval if we've completed all steps
+        clearInterval(interpolationTimer);
       }
-  
-      executeStep(0);
-    });
+    }, intervalDuration);
   }
