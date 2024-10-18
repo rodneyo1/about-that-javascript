75316ffd34e61c1f88f205fce186a7797ef0c2c5
rodnochieng <rodneyotieno27@gmail.com>
2024-10-18 14:25:00 +0300

    create queryservers,search multiple servers with a timeout

---
+++ gougle-search.js
@@ -0,0 +1,27 @@
+// Query a single server with a backup
+const queryServers = (serverName, q) => {
+    const mainUrl = `/${serverName}?q=${q}`;
+    const backupUrl = `/${serverName}_backup?q=${q}`;
+    
+    // return Promise.race([
+    //   getJSON(mainUrl),
+    //   getJSON(backupUrl)
+    // ]);
+  };
+  const gougleSearch = async (q) => {
+    const servers = ['web', 'image', 'video'];
+    
+    const searchPromise = Promise.all(
+      servers.map(server => 
+        queryServers(server, q).then(result => [server, result])
+      )
+    ).then(results => 
+      Object.fromEntries(results)
+    );
+  
+    const timeoutPromise = new Promise((_, reject) => {
+      setTimeout(() => reject(new Error('timeout')), 80);
+    });
+  
+    return Promise.race([searchPromise, timeoutPromise]);
+  };
