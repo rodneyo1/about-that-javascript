983b7d8d75090051fcad3dac352aeca1b94384d1
rodnochieng <rodneyotieno27@gmail.com>
2024-10-18 14:25:50 +0300

    return race promise

--- gougle-search.js
+++ gougle-search.js
@@ -3,18 +3,18 @@ const queryServers = (serverName, q) => {
     const mainUrl = `/${serverName}?q=${q}`;
     const backupUrl = `/${serverName}_backup?q=${q}`;
     
-    // return Promise.race([
-    //   getJSON(mainUrl),
-    //   getJSON(backupUrl)
-    // ]);
+    return Promise.race([
+      getJSON(mainUrl),
+      getJSON(backupUrl)
+    ]);
   };
   const gougleSearch = async (q) => {
     const servers = ['web', 'image', 'video'];
     
     const searchPromise = Promise.all(
-      servers.map(server => 
-        queryServers(server, q).then(result => [server, result])
-      )
+    //   servers.map(server => 
+    //     queryServers(server, q).then(result => [server, result])
+    //   )
     ).then(results => 
       Object.fromEntries(results)
     );
