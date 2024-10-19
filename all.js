0ab1c99a8f0d7e2fe58e8f5fb41904757fae1ffe
rod31 <rodneyotieno27@gmail.com>
2024-10-19 22:42:15 +0300

    handle empty objects

--- all.js
+++ all.js
@@ -1,12 +1,31 @@
-const all = (obj) => {
-    const keys = Object.keys(obj);
-    const promises = Object.values(obj);
-  
-    return Promise.all(promises).then(results => {
-      
-      return keys.reduce((acc, key, index) => {
-        acc[key] = results[index];
-        return acc;
-      }, {});
+function all(promisesObj) {
+  const keys = Object.keys(promisesObj);
+
+  // If the object is empty, return a resolved promise with an empty object
+  if (keys.length === 0) {
+    return Promise.resolve({});
+  }
+
+  const values = Object.values(promisesObj);
+
+  return Promise.all(values).then(resolvedValues => {
+    const result = {};
+    keys.forEach((key, index) => {
+      result[key] = resolvedValues[index];
     });
-  };
+    return result;
+  });
+}
+
+// const emptyObject = {};
+
+// all(emptyObject).then(result => console.log(result)); 
+
+// const promises = {
+//   first: Promise.resolve(1),
+//   second: Promise.resolve(2),
+//   third: new Promise(resolve => setTimeout(() => resolve(3), 1000))
+// };
+
+// all(promises).then(result => console.log(result));
+// // Output: { first: 1, second: 2, third: 3 }
